b689710a8f3bb0e4735ec04767824000a5a97bfb
Kamil Kucharski <kamil.kucharski@makimo.pl>
2021-10-16 10:44:07 +0200

    Add development startup file

---
+++ backend/main/development.py
@@ -0,0 +1,23 @@
+# pylint: disable=C0321,C0413
+from gevent import monkey; monkey.patch_all()
+import gevent
+from werkzeug.serving import run_simple
+
+from .apps import create_cards_app
+
+
+GATEWAY_PORT = 9000
+
+
+if __name__ == '__main__':
+    gevent.joinall([
+        gevent.spawn(
+            run_simple,
+            '0.0.0.0',
+            GATEWAY_PORT,
+            create_cards_app(),
+            use_reloader=True,
+            use_debugger=True,
+            threaded=True,
+        ),
+    ])
