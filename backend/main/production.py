056c99f6d29655616cc1f029bf2e7893aa941843
Kamil Kucharski <kamil.kucharski@makimo.pl>
2021-10-16 11:19:38 +0200

    Format with black

--- backend/main/production.py
+++ backend/main/production.py
@@ -1,5 +1,7 @@
 # pylint: disable=C0321,C0413,C0411,E0401,C0412
-from gevent import monkey; monkey.patch_all()
+from gevent import monkey
+
+monkey.patch_all()
 
 import os
 
@@ -9,17 +11,21 @@ from gevent.pywsgi import WSGIServer
 from .apps import create_cards_app_with_proxy
 
 
-GATEWAY_HOST = os.environ.get('GATEWAY_HOST', '0.0.0.0')
-GATEWAY_PORT = os.environ.get('GATEWAY_PORT', 9000)
+GATEWAY_HOST = os.environ.get("GATEWAY_HOST", "0.0.0.0")
+GATEWAY_PORT = os.environ.get("GATEWAY_PORT", 9000)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    http_server = WSGIServer((GATEWAY_HOST, GATEWAY_PORT), create_cards_app_with_proxy())
+    http_server = WSGIServer(
+        (GATEWAY_HOST, GATEWAY_PORT), create_cards_app_with_proxy()
+    )
 
     try:
-        gevent.joinall([
-            gevent.spawn(http_server.serve_forever),
-        ])
+        gevent.joinall(
+            [
+                gevent.spawn(http_server.serve_forever),
+            ]
+        )
     except KeyboardInterrupt:
-        print('Exiting')
+        print("Exiting")
